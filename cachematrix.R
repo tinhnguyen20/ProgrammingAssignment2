19bc6db1b16cce847138a661136a95487214050d
Tinh Nguyen <hungrytinh@gmail.com>
2014-05-24 00:20:58 -0700

    Update cachematrix.R
    Turning in assignment, trial 1

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+cache = NULL
+  set <- function(y){
+    x <<- y 
+    cache <<- NULL
+  }
+  get <- function(){
+    x
+  }
+  setInverse <- function(){
+    cache <<- solve(x)
+  }
+  getInverse <- function(){
+    cache
+  }
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+   m <- x$getInverse()
+  if(!is.null(m)){
+    solve(x$get())
+  }
+  else{
+    m
+  }
